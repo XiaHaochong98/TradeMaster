e6972226685c3c58e4b319f10e0370cccc79f02c
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-02-08 20:59:35 +0800

    add timegan-pytorch

--- data_synthesis/timegan-pytorch/metrics/metric_utils.py
+++ data_synthesis/timegan-pytorch/metrics/metric_utils.py
@@ -329,6 +329,7 @@ def post_hoc_discriminator(ori_data, generated_data):
     args["padding_value"] = -1.0
     args["max_seq_len"] = 100
     args["padding_value"]=-1.0
+    args["train_rate"] = 0.8
 
     ori_data,ori_time=ori_data
     generated_data,generated_time=generated_data
