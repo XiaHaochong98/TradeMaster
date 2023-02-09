7afea8a9e92efb965e834158a851412699afef3b
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-02-09 19:22:09 +0800

    add timegan-pytorch

--- data_synthesis/timegan-pytorch/metrics/metric_utils.py
+++ data_synthesis/timegan-pytorch/metrics/metric_utils.py
@@ -327,7 +327,7 @@ def post_hoc_discriminator(ori_data, generated_data):
     args["model_type"] = "gru"
     args["epochs"] = 250
     args["batch_size"] = 128
-    args["num_layers"] = 6
+    args["num_layers"] = 3
     args["padding_value"] = -1.0
     args["max_seq_len"] = 24
     args["train_rate"] = 0.8
